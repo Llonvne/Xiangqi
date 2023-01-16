a81fc7a3bccddc70a6b17d71c5e363945ea5dc1d
Llonvne <work@llonvne.cn>
2023-01-16 16:40:23 +0800

    filecode

--- src/main/kotlin/pieces/types/Advisor.kt
+++ src/main/kotlin/pieces/types/Advisor.kt
@@ -2,12 +2,12 @@ package pieces.types
 
 import board.Point
 import pieces.Piece
-import pieces.PieceColor
 import pieces.PiecePrototype
 import pieces.PieceType
 
 object Advisor : PiecePrototype(PieceType.Advisor) {
     override fun getAvailablePoint(piece: Piece, point: Point, board: Map<Point, Piece>): Set<Point> {
-        if piece.pColor == PieceColor.Black
+        // TODO
+        throw NotImplementedError()
     }
 }
